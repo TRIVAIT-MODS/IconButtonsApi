93375a29c86929afe9aee31c3b8f5b82d4fe9457
TRIVAIT <dimitmapper@gmail.com>
2026-06-19 12:53:29 +0400

    initial commit

---
+++ src/main/java/org/trivait/iba/IconButtonsApi.java
@@ -0,0 +1,16 @@
+package org.trivait.iba;
+
+import net.fabricmc.api.ClientModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IconButtonsApi implements ClientModInitializer {
+	public static final String MOD_ID = "iconbuttonsapi";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
